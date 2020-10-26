2d9f5310a17ad5fe7b337a0ecf22ba55649684f3
T.I <T.I@TI.rsatacadao.local>
2020-10-26 13:53:54 -0300

    criação do arquivo classe UserServlet

---
+++ CadastroUsuarioJSP/src/br/com/java/web/UserServlet.java
@@ -0,0 +1,52 @@
+package br.com.java.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.java.model.dao.UsuarioDAO;
+
+public class UserServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private UsuarioDAO dao;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+
+		try {
+			switch (action) {
+			case "/new":
+				mostrarNovoForm(request, response);
+				break;
+			case "/insert":
+				inserir(request, response);
+				break;
+			case "/delete":
+				deleteUser(request, response);
+				break;
+			case "/edit":
+				showEditForm(request, response);
+				break;
+			case "/update":
+				updateUser(request, response);
+				break;
+			default:
+				listUser(request, response);
+				break;
+			}
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}
+	}
+
+}
