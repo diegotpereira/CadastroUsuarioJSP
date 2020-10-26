31ba9f299f27411b9f356a427b1ba17d6fed5481
T.I <T.I@TI.rsatacadao.local>
2020-10-26 11:31:26 -0300

    criação do arquivo classe usuario.java

---
+++ CadastroUsuarioJSP/src/br/com/java/model/Usuario.java
@@ -0,0 +1,43 @@
+package br.com.java.model;
+
+public class Usuario {
+	
+	protected int id;
+	protected String nome;
+	protected String email;
+	protected int telefone;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	protected String nacionalidade;
+
+}
